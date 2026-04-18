44fa0b96cc1a37cdbcf516f5b5fec16a2707776e
Carlo Gonzalez <150742193+Carl0gonzalez@users.noreply.github.com>
2026-04-18 17:17:28 -0400

    chore: eliminar TransactionRequest.kt obsoleto (reemplazado por ApiTransaction)

--- app/src/main/java/com/cjgr/awandroide/data/remote/model/TransactionRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.cjgr.awandroide.data.remote.model
-
-data class TransactionRequest(
-    val userId: Int,
-    val fecha: String,
-    val monto: Double,
-    val descripcion: String,
-    val tipo: String = "transferencia"
-)
